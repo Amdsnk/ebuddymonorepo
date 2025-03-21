590f7bdce79fabe0bf9e99e6a0bc254fea182b48
Amanda Angela Soenoko <75839443+Amdsnk@users.noreply.github.com>
2025-03-21 23:13:49 +0700

    Update layout.tsx

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -2,14 +2,9 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ThemeRegistry } from "@/theme/ThemeRegistry"
-import dynamic from "next/dynamic"
+import { Providers } from "@/store/provider"
 
 const inter = Inter({ subsets: ["latin"] })
-
-// Dynamically import the Providers component with no SSR
-const ClientProviders = dynamic(() => import("@/store/provider").then((mod) => mod.Providers), {
-  ssr: false,
-})
 
 export const metadata: Metadata = {
   title: "EBuddy App",
@@ -25,7 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeRegistry>
-          <ClientProviders>{children}</ClientProviders>
+          {/* The Provider will only be used on the client */}
+          <Providers>{children}</Providers>
         </ThemeRegistry>
       </body>
     </html>
