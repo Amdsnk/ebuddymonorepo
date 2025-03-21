3218cfdaea3ad5e775e1da6608f57a43a29f24e1
Amanda Angela Soenoko <75839443+Amdsnk@users.noreply.github.com>
2025-03-21 16:22:31 +0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import UpdateButton from "../apps/frontend/src/components/atoms/UpdateButton"
+
+export default function SyntheticV0PageForDeployment() {
+  return <UpdateButton />
+}
+
