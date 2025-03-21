8dfba552e958097cad832e7fc2c6e863d23cfc71
Amanda Angela Soenoko <75839443+Amdsnk@users.noreply.github.com>
2025-03-21 23:59:14 +0700

    Create page.tsx

---
+++ apps/frontend/src/app/test-page/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Test Page</h1>
+      <p>If you can see this, basic rendering is working.</p>
+    </div>
+  )
+}
+
